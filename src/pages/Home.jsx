3119240e692adfa741e877a5f59a83b20b9fee22
Abhinav Jain <jabhinav041@gmail.com>
2024-05-09 14:07:31 +0530

    fetch and show data and implemented error catching

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,10 @@
 import { useState } from 'react';
+import { searchForShows } from '../api/tvmaze';
 
 function Home() {
   const [searchStr, setSearchStr] = useState('');
+  const [apiData, setApiData] = useState(null);
+  const [apiDataError, setApiDataError] = useState(null);
 
   const onInputChange = event => {
     setSearchStr(event.target.value);
@@ -10,11 +13,27 @@ function Home() {
   const onSearch = async event => {
     event.preventDefault();
 
-    const response = await fetch(
-      `https://api.tvmaze.com/search/shows?q=${searchStr}`
-    );
-    const body = await response.json();
-    console.log(body);
+    try {
+      setApiDataError(null);
+      const result = await searchForShows(searchStr);
+      setApiData(result);
+    } catch (error) {
+      setApiDataError(error);
+    }
+  };
+
+  const renderApiData = () => {
+    if (apiDataError) {
+      return <div>Error occured:-{apiDataError.message}</div>;
+    }
+
+    if (apiData) {
+      return apiData.map(data => (
+        <div key={data.show.id}>{data.show.name}</div>
+      ));
+    }
+
+    return null;
   };
 
   return (
@@ -23,6 +42,8 @@ function Home() {
         <input type="text" value={searchStr} onChange={onInputChange} />
         <button type="submit">Search</button>
       </form>
+
+      {renderApiData()}
     </div>
   );
 }
